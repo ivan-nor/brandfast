e841f70cb64134be118d2447128bdec5b17b20a2
Ivan Nor <ivan.norik@gmail.com>
2025-02-05 17:42:57 +0300

    [develop] feat: add product page

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,24 +1,30 @@
-const reviewsSwiper = new Swiper('.reviews__slider', {
+const productReviewsSwiper = new Swiper('.product-reviews__slider', {
   loop: true,
   slidesPerView: 3,
   width: 606,
   spaceBetween: 50,
-  // autoHeight: true,
   centeredSlides: true,
-  // breakpoints: {
-  //   980: {
-  //     width: 400,
-  //   },
-  //   1200: {
-  //     width: 560,
-  //   }
-  // },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
 });
+const productReviewsNext = document.querySelector('.product-reviews__slider .reviews__arrow-button-next')
+const productReviewsPrev = document.querySelector('.product-reviews__slider .reviews__arrow-button-prev')
+productReviewsNext.addEventListener('click', () => productReviewsSwiper.slideNext(400))
+productReviewsPrev.addEventListener('click', () => productReviewsSwiper.slidePrev(400))
 
+const reviewsSwiper = new Swiper('.reviews__slider', {
+  loop: true,
+  slidesPerView: 3,
+  width: 606,
+  spaceBetween: 50,
+  centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
 const reviewsNext = document.querySelector('.reviews__arrow-button-next')
 const reviewsPrev = document.querySelector('.reviews__arrow-button-prev')
 reviewsNext.addEventListener('click', () => reviewsSwiper.slideNext(400))
